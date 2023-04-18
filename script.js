5092931ad901ee36a6ac6d25240739f2d363316e
mahendra4711 <126939275+mahendra4711@users.noreply.github.com>
2023-04-18 10:43:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+/*----------sticky-header-----------*/
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+        $('.site-header').addClass("sticky-header");
+    } else {
+        $('.site-header').removeClass("sticky-header");
+    }
+});
+
+
+
+/*----------Menu Toggle----------*/
+
+
+ $(".menu-toggle").click(function() {
+        $(this).toggleClass("on");
+        $(".navbar").slideToggle();
+    });
+
+
+
+/*-----------Responsive-Tab-------------*/
+
+
+$(document).ready(function() {
+    $('#responsiveTabsDemo').responsiveTabs({
+        startCollapsed: 'accordion'
+    });
+});
+
+
+
+/*-------Scroll to top----------*/
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.top').fadeIn();
+    } else {
+        $('.top').fadeOut();
+    }
+});
+$('.top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+
+
+
+/*----------- Accordian-------------*/
+
+
+$(".about__vision h2").click(function() {
+    $(this).next().slideToggle();
+     $(this).toggleClass('active');
+});
+
+
+
+/*-------Number-count--------*/
+
+
+$('.count').each(function() {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function(now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
