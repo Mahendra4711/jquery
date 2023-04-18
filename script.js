518451652a83bc63ee5bde9c1fa9c2d8dccd273b
mahendra4711 <126939275+mahendra4711@users.noreply.github.com>
2023-04-18 12:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 /*----------sticky-header-----------*/
 
+
+header{
+    position:fixed;
+    width: 100%;
+    background: #2F5349;
+    color: #fff;
+    font-family: 'PT Sans', sans-serif;
+    transition: all 0.4s ease;
+    z-index: 99999;
+  }
+header.sticky {
+    background: rgba(47, 83, 73,0.8);
+    backdrop-filter: blur(5px);
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    padding: 10px 0;
+
+  }
+  
+ 
+.site-header {
+    padding: 30px 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    background-color: #2F5349;
+    position:fixed;
+    transition: all 0.4s ease;
+}
+
+.site-header .wrap {
+    max-width: 1400px;
+}
+
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 1) {
-        $('.site-header').addClass("sticky-header");
-    } else {
-        $('.site-header').removeClass("sticky-header");
-    }
-});
+		if ($(this).scrollTop() > 1){  
+			$('.site-header').addClass("sticky");
+		  }
+		  else{
+			$('.site-header').removeClass("sticky");
+		  }
+	});
 
 
 
