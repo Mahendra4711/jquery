19c6796e2a2d99b445681447c3ec5c9615db6c3b
mahendra4711 <126939275+mahendra4711@users.noreply.github.com>
2023-04-19 17:31:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,18 +68,55 @@ $(document).ready(function() {
 /*-------Scroll to top----------*/
 
 
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-        $('.top').fadeIn();
-    } else {
-        $('.top').fadeOut();
-    }
-});
-$('.top').click(function() {
-    $("html, body").animate({ scrollTop: 0 }, 600);
-    return false;
-});
 
+<a href="#" id="scroll" style="display: none;"><span></span></a>
+
+	
+#scroll {
+    position:fixed;
+    right:10px;
+    bottom:10px;
+    cursor:pointer;
+    width:50px;
+    height:50px;
+    background-color:#3498db;
+    text-indent:-9999px;
+    display:none;
+    -webkit-border-radius:60px;
+    -moz-border-radius:60px;
+    border-radius:60px
+}
+#scroll span {
+    position:absolute;
+    top:50%;
+    left:50%;
+    margin-left:-8px;
+    margin-top:-12px;
+    height:0;
+    width:0;
+    border:8px solid transparent;
+    border-bottom-color:#ffffff;
+}
+#scroll:hover {
+    background-color:#e74c3c;
+    opacity:1;filter:"alpha(opacity=100)";
+    -ms-filter:"alpha(opacity=100)";
+}
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
 
 
 /*----------- Accordian-------------*/
